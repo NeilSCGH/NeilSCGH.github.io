d3366b4259bf9cc17443913b3781a5d679505130
NeilSCGH <neil.segard@gmail.com>
2020-09-15 21:40:11 +0200

    qr codes added

---
+++ QRCodes/images/generator.py
@@ -0,0 +1,11 @@
+import qrcode
+import os
+
+currentPath= os.getcwd()
+
+for i in range(0,10):
+    name="https://neilscgh.github.io/QRCodes/{}.html".format(i)
+    img = qrcode.make(name)
+    img.save("{}\\{}.png".format(currentPath,i))
+
+print("Done")
