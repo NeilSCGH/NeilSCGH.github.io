c3feab55f3cd021694f98b6122dd2504ae17dad3
NeilSCGH <neil.segard@gmail.com>
2020-09-15 21:50:09 +0200

    maj

--- QRCodes/images/generator.py
+++ QRCodes/images/generator.py
@@ -3,7 +3,7 @@ import os
 
 currentPath= os.getcwd()
 
-for i in range(0,10):
+for i in range(0,5):
     name="https://neilscgh.github.io/QRCodes/{}.html".format(i)
     img = qrcode.make(name)
     img.save("{}\\{}.png".format(currentPath,i))
