0d3d1cb013e3263fdb2ec973c42c451c4737a0a9
NeilSCGH <neil.segard@gmail.com>
2020-11-02 15:50:35 +0100

    maj

--- QRCodes/images/generator.py
+++ QRCodes/images/generator.py
@@ -3,9 +3,10 @@ import os
 
 currentPath= os.getcwd()
 
-for i in range(0,5):
+for i in range(1,100):
     name="https://neilscgh.github.io/QRCodes/{}.html".format(i)
     img = qrcode.make(name)
     img.save("{}\\{}.png".format(currentPath,i))
+    print("{}\\{}.png created".format(currentPath,i))
 
 print("Done")
